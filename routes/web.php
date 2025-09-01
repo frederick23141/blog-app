dc31583f76af4bb22892bef2b50da376e091db8c
Federico Toro Restrepo <federicotoro23141@gmail.com>
2025-09-01 17:09:06 -0500

    rutas en orden especifico

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+//las rutas deben ser en orden evaluadas en orden lineal
+
+Route::get(uri: '/', action: function () {
     return view('welcome');
 });
+
+Route::get(uri: '/posts', action: function () {
+    return 'aca se muestran los posts';
+});
+
+//crear rutas para los metodos HTTP
+Route::get(uri: '/posts/create', action: function(){
+    return 'Aqui se muestra el formulario para crear un post';
+});
+
+
+//valor opcional con el caracter ?
+Route::get(uri: '/posts/{post}', action: function ($post) {
+     return "Mostrando el post: {$post} ";
+});
+
+
+
+//get
+//post
+//put
+//patch
+//delete
+//options
